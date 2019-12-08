f4f9be313a88517692b3794dc351284eb085325c
seguthart <seguthart@gmail.com>
2019-12-08 17:17:19 -0500

    updated application structure to use blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,14 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
-app = Flask(__name__)
+bootstrap = Bootstrap()
 
-from app import routes
+def create_app():
+    app = Flask(__name__)
+
+    bootstrap.init_app(app)
+
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    return app
